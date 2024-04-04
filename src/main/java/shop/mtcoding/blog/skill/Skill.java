e56931a546f28cf7bd3c967a37823f807b458db3
hyeonnnit <153695703+hyeonnnit@users.noreply.github.com>
2024-04-04 10:27:59 +0900

    skill ì 테이블 (#1)

--- src/main/java/shop/mtcoding/blog/skill/Skill.java
+++ src/main/java/shop/mtcoding/blog/skill/Skill.java
@@ -1,8 +1,14 @@
 package shop.mtcoding.blog.skill;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import shop.mtcoding.blog.post.Post;
+import shop.mtcoding.blog.resume.Resume;
+
+import java.sql.Timestamp;
 
 @NoArgsConstructor
 @Table(name = "skill_tb")
@@ -12,5 +18,34 @@ public class Skill {
     @Id // PK 설정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment 전략
     private Integer id;
+    private String skill;
+
+    @JoinColumn(name = "resume_id", nullable = true)
+    @ManyToOne(fetch =  FetchType.LAZY)
+    private Resume resume; //null
+
+    @JoinColumn(name = "post_id", nullable = true)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post; // null 허용 되어야 한다
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Builder
+    public Skill(Integer id, String skill, Resume resume, Post post, Timestamp createdAt) {
+        this.id = id;
+        this.skill = skill;
+        this.resume = resume;
+        this.post = post;
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Skill{" +
+                "id=" + id +
+                ", skill='" + skill + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 
 }
